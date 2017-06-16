1026e24d3e6e63a7746f83fa3d491ab8894ff8aa
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-06-16 14:13:20 +0200

    Reorganised command for setting TrainzUtil PATH

--- TrainzUtil-Path.cmd
+++ TrainzUtil-Path.cmd
@@ -1,9 +1,10 @@
 @ECHO OFF
-WHERE TrainzUtil
-IF %ERRORLEVEL% EQU 0 GOTO printTrainzUtil
 IF "%~1"=="" (
+    WHERE TrainzUtil
+    IF %ERRORLEVEL% EQU 0 GOTO last
     IF EXIST "C:\Program Files\N3V Games\Trainz A New Era" (
         CALL "%~0" "C:\Program Files\N3V Games\Trainz A New Era"
+        GOTO last
     ) ELSE (
         ECHO.No common TrainzUtil directory detected. Try executing the script with a custom TrainzUtil directory path as argument.
         EXIT /B %ERRORLEVEL%
@@ -17,3 +18,4 @@ ECHO.Expanding PATH with %~1
 SET PATH=%PATH%;%~1
 :printTrainzUtil
 WHERE TrainzUtil
+:last
