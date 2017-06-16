4bdab1d9baeac86dc6a0e4e1346ddd6efa65e04a
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-06-16 14:25:45 +0200

    Reorganised command for setting TrainzUtil PATH

--- TrainzUtil-Path.cmd
+++ TrainzUtil-Path.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
+WHERE TrainzUtil 2>nul 1>nul
+IF %ERRORLEVEL% EQU 0 GOTO last
 IF "%~1"=="" (
-    WHERE TrainzUtil
-    IF %ERRORLEVEL% EQU 0 GOTO last
     IF EXIST "C:\Program Files\N3V Games\Trainz A New Era" (
         CALL "%~0" "C:\Program Files\N3V Games\Trainz A New Era"
         GOTO last
