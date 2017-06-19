af9e51b74fea7e61b2e215e72d6ca72001a7942b
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-06-20 00:00:29 +0200

    Common Build script for assets

---
+++ assets/src/build.cmd
@@ -0,0 +1,31 @@
+@ECHO OFF
+ECHO.WHERE TrainzUtil
+WHERE TrainzUtil
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO.TrainzUtil not in PATH! >&2
+	ECHO.Run TrainzUtil-Path.cmd script or include TrainzUtil from your Trainz Simulator distribution into your PATH variable >&2
+	EXIT /B %ERRORLEVEL%
+)
+
+REM Change to current directory
+PUSHD "%~dp0"
+FOR /D %%A IN (*) DO (
+    PUSHD "%%~A"
+    ECHO.excludefiles.txt > excludefiles.txt
+    ECHO.%~nx0 >> excludefiles.txt
+
+    MD "..\..\bin\%%~A"
+    XCOPY "." "..\..\bin\%%~A" /E /C /H /Y /EXCLUDE:excludefiles.txt
+    COPY /Y "..\PermitManagerShared.gs" "..\..\bin\%%~A"
+
+    PUSHD "..\..\bin\%%~A"
+    FOR /R . %%G IN (*.gs) DO (
+        TrainzUtil compile -i"..\..\..\ref\TS12\scripts" -o"%%~dpnG" "%%~G"
+    )
+    POPD
+
+    POPD
+)
+:cleanup
+REM Restore current directory location prior to invoking this script
+POPD
